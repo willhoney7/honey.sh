dfbee67178313a3db2490186163f393111ff19a4
Will Honey <a+code@honey.sh>
2018-12-12 21:08:08 -0700

    Update readme, package.json info, and about-website page.

--- src/pages/about-website.js
+++ src/pages/about-website.js
@@ -18,18 +18,24 @@ const AboutWebsite = () => (
                 .
             </li>
             <li>
-                The second iteration (2012) was written with{' '}
+                The{' '}
+                <a href="https://v2.honey.sh" target="_blank" rel="noreferrer noopener">
+                    second iteration
+                </a>{' '}
+                (2012) was written with{' '}
                 <a href="https://jekyllrb.com/" target="_blank" rel="noreferrer noopener">
                     Jekyll
                 </a>{' '}
-                and hosted on Github Pages.
+                and hosted on Github Pages. I had grand plans of blogging but never followed
+                through.
             </li>
             <li>
                 This latest version (2018) was built with{' '}
                 <a href="https://www.gatsbyjs.org/" rel="noreferrer noopener">
                     Gatsby
                 </a>
-                . It is hosted by <a href="https://netlify.com">Netlify</a>.
+                . It is hosted by <a href="https://netlify.com">Netlify</a>. I'm not quite sure
+                where I'll take it.
             </li>
         </ul>
         <Link to="/">Go back to the homepage</Link>
